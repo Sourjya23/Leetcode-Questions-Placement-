ad34d5bf93133f11743a682e48486594c5a9bc6b
Sourjya Mitra <162135446+Sourjya23@users.noreply.github.com>
2026-07-08 13:46:40 +0530

    Time: 1 ms (40.9%), Space: 63.6 MB (7.99%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k%n;
+        int[] temp = Arrays.copyOfRange(nums,n-k,n);
+
+        for(int i=n-k-1;i>=0;i--){
+            nums[i+k]=nums[i];
+        }
+        for(int i=0;i<k;i++){
+            nums[i]=temp[i];
+        }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
