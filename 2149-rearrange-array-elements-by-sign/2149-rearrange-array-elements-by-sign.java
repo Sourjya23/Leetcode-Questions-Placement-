adc63b8c16c9f6871b7f6e1e99d34dc53b00f373
Sourjya Mitra <162135446+Sourjya23@users.noreply.github.com>
2026-07-22 06:55:43 +0530

    Time: 3 ms (100%), Space: 174.9 MB (79.7%) - LeetHub

---
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        int posIdx=0,negIdx=1;
+
+        for(int i =0;i<n;i++){
+            if(nums[i]<0){
+                result[negIdx]+=nums[i];
+                negIdx+=2;
+            }else{
+                result[posIdx]+=nums[i];
+                posIdx+=2;
+            }
+        }
+        return result;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
