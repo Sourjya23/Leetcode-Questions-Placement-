bbb1ddc91e49cb156c56042269db4adce8fd835c
Sourjya Mitra <162135446+Sourjya23@users.noreply.github.com>
2026-08-03 23:57:11 +0530

    Time: 0 ms (100%), Space: 43.2 MB (25.38%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -0,0 +1,38 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        ArrayList result = new ArrayList<>();
+
+        int top =0;
+        int bottom = matrix.length-1;
+        int left =0;
+        int right = matrix[0].length-1;
+
+        while(top<=bottom && left<=right){
+            for(int i=left;i<=right;i++){
+                result.add(matrix[top][i]);
+            }
+            top++;
+        for(int i = top;i<=bottom;i++){
+            result.add(matrix[i][right]);
+        }
+        right--;
+        if(top<=bottom){
+            for(int i=right;i>=left;i--){
+                result.add(matrix[bottom][i]);
+            }
+        }
+        bottom--;
+        if(left<=right){
+            for(int i=bottom;i>=top;i--){
+                result.add(matrix[i][left]);
+            }
+        }
+        left++;
+        }
+        return result;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
